54a8525a09f6dd67d806161f76190bf1fae56bbd
WithyouTt <39683803+jimmg5202@users.noreply.github.com>
2024-01-26 17:32:39 +0800

    Update redbook21.js

--- redbook21.js
+++ redbook21.js
@@ -148,7 +148,7 @@ if (body) {
             try {
                 let q = JSON.parse(body);
                 q.data = q.data.filter(e => {
-                    return (!e.is_ads && e.model_type === 'note' && e.live_status =!2);
+                    return (!e.hasOwnProperty('live') && !e.is_ads && e.model_type === 'note' );
                 });
                 body = JSON.stringify(q);
             } catch (E) {
